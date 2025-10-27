16b61682a36139e9cfe61ac6df31e52200f0eb66
lluizcosta <luizzeduardocosta@gmail.com>
2025-10-27 20:00:08 -0300

    Menu Principal v1

---
+++ Assets/Script/MainMenu/ScriptMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("flappy b");
+    }
+
+    public void exitGame()
+    {
+        Debug.Log("Saindo do jogo...");
+
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
